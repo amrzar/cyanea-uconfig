a0c5c07b0a93ad0b3f28fcb6c50f5a8897d888ac
Amirreza Zarrabi <amrzar@gmail.com>
2021-11-11 15:19:08 +1100

    update headers with SPDX-License-Identifier
    Signed-off-by: Amirreza Zarrabi <amrzar@gmail.com>

--- config.utils.h
+++ config.utils.h
@@ -1,19 +1,4 @@
-/*
- * Copyright 2020 Amirreza Zarrabi <amrzar@gmail.com>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * THIS IS ORIGINALLY COPIED FROM LINUX KERNEL.
- *
- */
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 
 #ifndef __CONFIG_UTILS_H__
 #define __CONFIG_UTILS_H__
